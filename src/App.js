a7bc4d210b559d5c874c7dd717af7b16fe918e26
Amy Gori <amydgori@gmail.com>
2022-09-19 15:41:06 -0400

    Add input field to demo forms in React

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import DeveloperDetail from './components/DeveloperDetail'
 import { useEffect } from 'react'
 import axios from 'axios'
 import 'cirrus-ui'
+import { InputField } from './components/Forms'
 
 export const App = () => {
   const [selectedDev, setSelectedDev] = useState(null)
   const [devs, setDevs] = useState([])
+  const showForms = true
 
   useEffect(() => {
     const url = 'https://node-api-devs-for-hire.glitch.me/devs'
     axios.get(url).then((res) => setDevs(res.data.data))
   }, [])
+
+  if (showForms) {
+    return <InputField />
+  }
 
   if (selectedDev) {
     return (
---
+++ src/components/Forms.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+export const InputField = () => {
+  const [username, setUsername] = useState('')
+
+  const handleChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1> Smol Form</h1>
+      <input type="text" value={username} onChange={handleChange} />
+    </div>
+  )
+}
